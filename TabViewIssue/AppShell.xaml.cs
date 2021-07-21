b6b80dd46e20e298f644e2fed08449dea986c103
Nuno Bem <nunobem@gmail.com>
2021-07-21 19:41:32 +0000

    Add project files.

---
+++ TabViewIssue/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TabViewIssue
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ TabViewIssue/Views/ItemsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TabViewIssue.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        public ItemsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
